379bcc5f8c0e4e94083f0efcdc7afffeca784e0a
Pepe García <pepe@pepegar.com>
2021-08-31 11:53:12 +0100

    move scalastyle comment after lint

--- modules/engine/src/main/scala/higherkindness/droste/contrib/NewTypesSyntax.scala
+++ modules/engine/src/main/scala/higherkindness/droste/contrib/NewTypesSyntax.scala
@@ -4,6 +4,7 @@ import higherkindness.droste.util.newtypes.@@
 
 object NewTypesSyntax {
   implicit class NewTypesOps[A](a: A) {
-    def @@[B]: A @@ B = higherkindness.droste.util.newtypes.@@(a) //scalastyle:off
+    def @@[B]: A @@ B = //scalastyle:off
+      higherkindness.droste.util.newtypes.@@(a)
   }
 }
